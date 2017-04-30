5f60c93a9b70c18606786675084b7ecc6319d975
Soupbubbles <vattentrollet@gmail.com>
2017-04-30 16:07:31 +0100

    Added ConfigurationHandler

---
+++ src/main/java/soupbubbles/minecraftboom/handler/ConfigurationHandler.java
@@ -0,0 +1,51 @@
+package soupbubbles.minecraftboom.handler;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import soupbubbles.minecraftboom.reference.Reference;
+
+public class ConfigurationHandler
+{
+    public static Configuration configuration;
+
+
+    public static void init(File configFile)
+    {
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile, true);
+            loadConfiguration();
+        }
+    }
+
+    private static void loadConfiguration()
+    {
+        Settings.replaceVanillaStairRecipe = configuration.getBoolean(Settings.REPLACE_VANILLA_STAIR_RECIPE_NAME, configuration.CATEGORY_GENERAL, Settings.REPLACE_VANILLA_STAIR_RECIPE_DEFAULT, Settings.REPLACE_VANILLA_STAIR_RECIPE_COMMENT, Settings.REPLACE_VANILLA_STAIR_RECIPES_LABEL);
+
+        if (configuration.hasChanged())
+        {
+            configuration.save();
+        }
+    }
+
+    @SubscribeEvent
+    public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equalsIgnoreCase(Reference.MOD_ID))
+        {
+            loadConfiguration();
+        }
+    }
+
+    public static class Settings
+    {
+        public static boolean replaceVanillaStairRecipe;
+        private static final String REPLACE_VANILLA_STAIR_RECIPE_NAME = "Replace Vanilla Stair Recipe";
+        private static final String REPLACE_VANILLA_STAIR_RECIPES_LABEL = "replace_vanilla_stair_recipe.label";
+        private static final String REPLACE_VANILLA_STAIR_RECIPE_COMMENT = "Return true if the mod should override the default recipe for stairs. The new crafting recipe will yield 6 stair blocks instead of 4.";
+        private static final boolean REPLACE_VANILLA_STAIR_RECIPE_DEFAULT = true;
+    }
+}
--- src/main/java/soupbubbles/minecraftboom/proxy/CommonProxy.java
+++ src/main/java/soupbubbles/minecraftboom/proxy/CommonProxy.java
@@ -3,6 +3,7 @@ package soupbubbles.minecraftboom.proxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import soupbubbles.minecraftboom.handler.ConfigurationHandler;
 import soupbubbles.minecraftboom.init.ModBlocks;
 import soupbubbles.minecraftboom.init.ModItems;
 import soupbubbles.minecraftboom.init.ModRecipes;
@@ -12,9 +13,11 @@ public abstract class CommonProxy implements IProxy
     @Override
     public void preInit(FMLPreInitializationEvent event)
     {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        
         ModBlocks.registerBlocks();
         ModItems.registerItems();
-        ModRecipes.initRecipes();
+        ModRecipes.init();
     }
 
     @Override
