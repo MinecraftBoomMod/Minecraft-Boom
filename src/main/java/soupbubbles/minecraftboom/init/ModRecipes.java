053d23df08c51731c68b499fdc44f168eaab45b9
Soupbubbles <vattentrollet@gmail.com>
2017-04-30 16:08:11 +0100

    Updated crafting recipes for stairs

--- src/main/java/soupbubbles/minecraftboom/init/ModRecipes.java
+++ src/main/java/soupbubbles/minecraftboom/init/ModRecipes.java
@@ -6,50 +6,73 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import soupbubbles.minecraftboom.block.base.BlockStairBase;
+import soupbubbles.minecraftboom.handler.ConfigurationHandler;
 
 public class ModRecipes
 {
-
-    public static void initRecipes()
+    private static int stairOutputAmount = 4;
+    
+    public static void init()
     {
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_STONE, Blocks.STONE_SLAB, 3);
-
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_GRANITE, 2);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_DIORITE, 4);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_ANDESITE, 6);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_PRISMARINE_BRICKS, 1);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_DARK_PRISMARINE, 2);
+        if (ConfigurationHandler.Settings.replaceVanillaStairRecipe)
+        {
+            stairOutputAmount = 6;
+            replaceVanillaStairRecipe();
+        }
+        
+        initRecipes();
+    }
+    
+    private static void initRecipes()
+    {
+        addStairRecipe(Blocks.STONE, 0, ModBlocks.BLOCK_STAIRS_STONE);
+        addStairRecipe(2, ModBlocks.BLOCK_STAIRS_SMOOTH_GRANITE);
+        addStairRecipe(4, ModBlocks.BLOCK_STAIRS_SMOOTH_DIORITE);
+        addStairRecipe(6, ModBlocks.BLOCK_STAIRS_SMOOTH_ANDESITE);
+        addStairRecipe(1, ModBlocks.BLOCK_STAIRS_PRISMARINE_BRICKS);
+        addStairRecipe(2, ModBlocks.BLOCK_STAIRS_DARK_PRISMARINE);
         addStairRecipe(ModBlocks.BLOCK_STAIRS_END_BRICKS);
         addStairRecipe(ModBlocks.BLOCK_STAIRS_RED_NETHER_BRICK);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_PRISMARINE, 0);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_DARK_PRISMARINE, 1);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_END_STONE, 2);
-        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_NETHERRACK, 3);
+        addStairRecipe(ModBlocks.BLOCK_STAIRS_SMOOTH_PRISMARINE);
+        addStairRecipe(1, ModBlocks.BLOCK_STAIRS_SMOOTH_DARK_PRISMARINE);
+        addStairRecipe(2, ModBlocks.BLOCK_STAIRS_SMOOTH_END_STONE);
+        addStairRecipe(3, ModBlocks.BLOCK_STAIRS_SMOOTH_NETHERRACK);
+        
+        addStairSlabRecipe(Blocks.STONE_SLAB, 0, ModBlocks.BLOCK_STAIRS_STONE);
     }
 
+    private static void replaceVanillaStairRecipe()
+    {
+    }
+    
     private static void addStairRecipe(BlockStairBase output)
     {
-        addStairRecipe(output, 6, 0);
+        addStairRecipe(0, output);
+    }
+    
+    private static void addStairRecipe(int inputMeta, BlockStairBase output)
+    {
+        addStairRecipe(output.getStairBlockState().getBlock(), inputMeta, output);
     }
 
-    private static void addStairRecipe(BlockStairBase output, int meta)
+    private static void addStairRecipe(Block input, int inputMeta, BlockStairBase output)
     {
-        addStairRecipe(output, 6, meta);
+        addStairRecipe(input, inputMeta, output, stairOutputAmount);
+    }
+    
+    private static void addStairSlabRecipe(Block input, int inputMeta, BlockStairBase output)
+    {
+        addStairRecipe(input, inputMeta, output, stairOutputAmount / 2);
     }
 
-    private static void addStairRecipe(BlockStairBase output, int amount, int meta)
+    private static void addStairRecipe(Block input, int inputMeta, BlockStairBase output, int outputAmount)
     {
-        addStairRecipe(output, Item.getItemFromBlock(output.getStairBlockState().getBlock()), amount, meta);
+        addStairRecipe(new ItemStack(input, 1, inputMeta), new ItemStack(output, outputAmount, 0));
     }
-
-    private static void addStairRecipe(BlockStairBase output, Block input, int amount)
+    
+    private static void addStairRecipe(ItemStack input, ItemStack output)
     {
-        addStairRecipe(output, Item.getItemFromBlock(input), amount, 0);
-    }
-
-    private static void addStairRecipe(BlockStairBase output, Item input, int amount, int meta)
-    {
-        GameRegistry.addShapedRecipe(new ItemStack(output, amount, 0), "x  ", "xx ", "xxx", 'x', new ItemStack(input, 1, meta));
-        GameRegistry.addShapedRecipe(new ItemStack(output, amount, 0), "  x", " xx", "xxx", 'x', new ItemStack(input, 1, meta));
+        GameRegistry.addShapedRecipe(output, "x  ", "xx ", "xxx", 'x', input);
+        GameRegistry.addShapedRecipe(output, "  x", " xx", "xxx", 'x', input);
     }
 }
