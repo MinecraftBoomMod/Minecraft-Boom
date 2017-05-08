8229188d8b5f41c02642685977ae132e4427570b
Soupbubbles <vattentrollet@gmail.com>
2017-05-08 22:21:34 +0200

    Arrow texture

--- src/main/java/soupbubbles/minecraftboom/client/renderer/RenderPrismarineArrow.java
+++ src/main/java/soupbubbles/minecraftboom/client/renderer/RenderPrismarineArrow.java
@@ -4,6 +4,8 @@ import net.minecraft.client.renderer.entity.RenderArrow;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
+import soupbubbles.minecraftboom.reference.Assets;
+import soupbubbles.minecraftboom.reference.Reference;
 
 public class RenderPrismarineArrow extends RenderArrow
 {
@@ -15,6 +17,6 @@ public class RenderPrismarineArrow extends RenderArrow
 	@Override
 	protected ResourceLocation getEntityTexture(Entity entity) 
 	{
-		return new ResourceLocation("textures/entity/projectiles/arrow.png");
+		return new ResourceLocation(Reference.MOD_ID, Assets.TEXTURE_ENTITY_DIR + "prismarine_arrow.png");
 	}
 }
