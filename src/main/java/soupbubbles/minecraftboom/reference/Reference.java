3b83dfaba996de30c8ff82037e5b6a7742ad9930
Soupbubbles <vattentrollet@gmail.com>
2018-03-08 16:12:29 +0100

    Last changes

--- src/main/java/soupbubbles/minecraftboom/reference/Reference.java
+++ src/main/java/soupbubbles/minecraftboom/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
     public static final String MOD_ID = "minecraftboom";
     public static final String MOD_NAME = "Minecraft Boom";
     public static final String VERSION = "1.0";
-    public static final String ACCEPTED_VERSIONS = "1.11.2";
+    public static final String ACCEPTED_VERSIONS = "1.12.2";
 
     public static final String PROXY_CLIENT = "soupbubbles." + MOD_ID + ".proxy.ClientProxy";
     public static final String PROXY_SERVER = "soupbubbles." + MOD_ID + ".proxy.ServerProxy";
